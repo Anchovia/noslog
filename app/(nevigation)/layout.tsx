6f7a8b4baf439af1aa9a0bf9ed841bce2cd27dbd
Anchovia <hodolye@naver.com>
2025-09-11 10:53:30 +0900

    footer 추가

--- app/(nevigation)/layout.tsx
+++ app/(nevigation)/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/footer";
 import Header from "@/components/header";
 
 export default function NeviationLayout({
@@ -9,6 +10,7 @@ export default function NeviationLayout({
         <>
             <Header />
             {children}
+            <Footer />
         </>
     );
 }
