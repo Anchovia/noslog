1932892721642fac67814b2df3c40ef14b830d5f
Anchovia <hodolye@naver.com>
2026-07-22 02:58:36 +0900

    perf: 동적 라우터 캐시 재사용 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        staleTimes: {
+            dynamic: 300,
+        },
+    },
     async headers() {
         return [
             {
